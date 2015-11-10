665392b8298542b23541b19a8275a464ed8283b5
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-11-10 13:21:40 -0500

    Add select box that displays water use

---
+++ js/foods.js
@@ -0,0 +1,31 @@
+var foods = {
+  "apple": {
+    "food_name": "Apple",
+    "water_use": {
+      "gallons": {
+        "serving": "1 pound of apples",
+        "water": "84"
+      }
+    }
+  },
+
+  "avocado": {
+    "food_name": "Avocado",
+    "water_use": {
+      "gallons": {
+        "serving": "1 pound of avocados",
+        "water": "154"
+      }
+    }
+  },
+
+  "banana": {
+    "food_name": "Banana",
+    "water_use": {
+      "gallons": {
+        "serving": "1 pound of bananas",
+        "water": "103"
+      }
+    }
+  }
+}
---
+++ js/units.js
@@ -0,0 +1,5 @@
+var measuring_units = {
+  "gallons": {
+    label: "gallons of water"
+  }
+}
