a34e2ed98198e1ddbc43376b7375746278479937
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-11-11 21:22:49 -0500

    Add more food and beverage data

--- js/beverages.js
+++ js/beverages.js
@@ -1,5 +1,20 @@
 var beverages = {
-var beverages = {
+  "beverage": {
+    "food_name": "beverage",
+    "water_use": {
+      "water_litres": {
+        "per_litre": 1,
+        "per_serving": 1,
+        "serving": {
+          "description": "one glass of beverage",
+          "volume": {
+            "ml": 1,
+          }
+        }
+      }
+    }
+  },
+
   "apple_juice": {
     "food_name": "apple juice",
     "water_use": {
@@ -10,6 +25,53 @@ var beverages = {
           "description": "one glass of apple juice",
           "volume": {
             "ml": 230,
+          }
+        }
+      }
+    }
+  },
+
+  "beer": {
+    "food_name": "beverage",
+    "water_use": {
+      "water_litres": {
+        "per_litre": 298,
+        "per_serving": 74,
+        "serving": {
+          "description": "one glass of beer",
+          "volume": {
+            "ml": 250,
+          }
+        }
+      }
+    }
+  },
+
+  "milk": {
+    "food_name": "milk",
+    "water_use": {
+      "water_litres": {
+        "per_serving": 255,
+        "serving": {
+          "description": "one glass of milk",
+          "volume": {
+            "ml": 250,
+          }
+        }
+      }
+    }
+  },
+
+  "orange_juice": {
+    "food_name": "orange juice",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 1020,
+        "per_serving": 200,
+        "serving": {
+          "description": "one glass of orange juice",
+          "volume": {
+            "ml": 200,
           }
         }
       }
--- js/foods.js
+++ js/foods.js
@@ -1,4 +1,21 @@
 var foods = {
+
+  "food": {
+    "food_name": "food",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 1,
+        "per_serving": 1,
+        "serving": {
+          "description": "one serving",
+          "weight": {
+            "g": 1,
+          }
+        }
+      }
+    }
+  },
+
   "apple": {
     "food_name": "apple",
     "water_use": {
@@ -7,8 +24,8 @@ var foods = {
         "water": "84"
       },
       "water_litres": {
-        "litres_per_kg": 822,
-        "litres_per_serving": 125,
+        "per_kg": 822,
+        "per_serving": 125,
         "serving": {
           "description": "one apple",
           "weight": {
@@ -16,7 +33,7 @@ var foods = {
           }
         }
       }
-    },
+    }
   },
 
   "avocado": {
@@ -27,15 +44,185 @@ var foods = {
         "water": "154"
       }
     }
-  },
+  }
 
   "banana": {
-    "food_name": "Banana",
+    "food_name": "banana",
     "water_use": {
       "gallons": {
         "serving": "1 pound of bananas",
         "water": "103"
       }
+      "water_litres": {
+        "per_kg": 790,
+        "per_serving": 160,
+        "serving": {
+          "description": "one banana",
+          "weight": {
+            "g": 200,
+          }
+        }
+      }
     }
-  }
+  },
+
+  "beef": {
+    "food_name": "beef",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 15415,
+      }
+    }
+  },
+
+  "bread": {
+    "food_name": "bread (from wheat)",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 1608,
+      }
+    }
+  },
+
+  "butter": {
+    "food_name": "butter (from cow's milk)",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 5553
+      }
+    }
+  },
+
+  "cabbage": {
+    "food_name": "cabbage",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 237
+      }
+    }
+  },
+
+  "cheese": {
+    "food_name": "cheese",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 3178
+      }
+    }
+  },
+
+  "chicken": {
+    "food_name": "chicken (meat)",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 4325
+      }
+    }
+  },
+
+  "chocolate": {
+    "food_name": "chocolate",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 17196
+      }
+    }
+  },
+
+  "cucumber": {
+    "food_name": "cucumber",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 353
+      }
+    }
+  },
+
+  "dates": {
+    "food_name": "dates",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 2277
+      }
+    }
+  },
+
+  "eggs": {
+    "food_name": "eggs",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 3300,
+        "per_serving": 196,
+        "serving": {
+          "description": "one egg",
+          "weight": {
+            "g": 60,
+          }
+        }
+      }
+    }
+  },
+
+  "goat": {
+    "food_name": "goat (meat)",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 5521
+      }
+    }
+  },
+
+  "lettuce": {
+    "food_name": "lettuce",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 237
+      }
+    }
+  },
+
+  "corn": {
+    "food_name": "corn",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 1222
+      }
+    }
+  },
+
+  "milk": {
+    "food_name": "milk (from cow)",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 1020
+      }
+    }
+  },
+
+  "orange": {
+    "food_name": "orange",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 560
+      }
+    }
+  },
+
+  "pasta": {
+    "food_name": "pasta (dry, from wheat)",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 1849
+      }
+    }
+  },
+
+  "peach": {
+    "food_name": "peach or nectarine",
+    "water_use": {
+      "water_litres": {
+        "per_kg": 910
+      }
+    }
+  },
 }
