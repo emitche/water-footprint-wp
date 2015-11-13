5b84dbed16e8c99b64ff8c70f8973a87a75b40de
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-11-13 18:14:07 -0500

    Add serving function

--- js/units.js
+++ js/units.js
@@ -1,5 +1,23 @@
+var of_water = "of water";
+
 var measuring_units = {
-  "gallons": {
-    label: "gallons of water"
+  "g": {
+    label: "gram"
+  },
+
+  "gallon": {
+    label: "gallon"
+  },
+
+  "kg": {
+    label: "kilogram"
+  },
+
+  "l": {
+    label: "litre"
+  },
+
+  "lb": {
+    label: "pound"
   }
 }
