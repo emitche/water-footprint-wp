5aabb60d9f678864ed859333fcdbbc0d23cc6f64
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-11-19 12:43:23 -0500

    Refine text displayed

--- js/calculator.js
+++ js/calculator.js
@@ -38,7 +38,7 @@ function convert_kg_to_lb(n) {
 }
 
 function food_water_use(food_unit, water_amount, water_unit) {
-  text = "<div class='bulk-readout'><p>" + 1 + " " + food_unit + "</p><p>" + water_amount + " " + water_unit + " " + of_water + "</p></div>";
+  text = "<div class='bulk-readout'><p class='water-amount'>" + water_amount + " " + water_unit + " " + of_water + "</p><p>is used to produce " + 1 + " " + food_unit + "</p></div>";
   return text;
 }
 
