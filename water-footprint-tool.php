26b4650585ede1ecc294c4551e8537b58dc99c4d
Elizabeth Mitchell <elizabeth@sassafras.coop>
2015-11-28 00:18:05 -0500

    Shorten plugin name

--- water-footprint-tool.php
+++ water-footprint-tool.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * @package Water_Footprint_Tool
+ * @package Water_Footprint
  * @version 0.1
  */
 /*
-Plugin Name: Water Footprint Tool
-Plugin URI: http://github.com/emitche/water-footprint/
+Plugin Name: Water Footprint
+Plugin URI: http://github.com/emitche/water-footprint-wp/
 Description: A simple plugin to display the water footprint of specific foods.
 Author: Elizabeth Mitchell
 Version: 0.1
@@ -14,17 +14,17 @@ Author URI: http://github.com/emitche
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-function water_footprint_tool_shortcode() {
-    return "tool.php";
+function water_footprint_shortcode() {
+    return load_water_footprint();
 }
 
-function water_footprint_tool_register_shortcode() {
-    add_shortcode( 'water-footprint-tool', 'water_footprint_tool_shortcode' );
+function water_footprint_register_shortcode() {
+    add_shortcode( 'water-footprint', 'water_footprint_shortcode' );
 }
 
-add_action( 'init', 'water_footprint_tool_register_shortcode' );
+add_action( 'init', 'water_footprint_register_shortcode' );
 
-function load_water_footprint_tool(){
+function load_water_footprint(){
   ob_start();
   include('tool.php');
   return ob_get_clean();
