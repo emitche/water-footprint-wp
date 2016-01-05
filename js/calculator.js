803edb4ab6b81ddcb257dfcbf3827ed7f487111f
Elizabeth Mitchell <elizabeth@sassafras.coop>
2016-01-05 13:25:08 -0500

    3664: Ensure food item displays with autocomplete

--- js/calculator.js
+++ js/calculator.js
@@ -8,41 +8,10 @@ var display_unit = "gallon";
 var L_TO_GAL = 0.264172052358;
 var KG_TO_LB = 2.205;
 
-
-// Populates list of foods inside select
-
-$.each(foods, function(food, food_info) {
-  if (food_info.water_use.water_litres) {
-    $('.water-footprint .select-item').append(
-      "<option class='food-item " + food + "' value='" + food + "'>"
-      + food_info.food_name +
-      "</option>"
-    );
-  }
-});
-
-
-// Populates list of foods inside input
-
-var food_names = [];
-
-$.each(foods, function(food, food_info) {
-  if (food_info.water_use.water_litres) {
-    food_names.push(food_info.food_name);
-  }
-});
-
-$(".choose-item").autocomplete({
-  source: food_names,
-});
-
-
-
 function convert_units(n) {
   if (UNITS == "us") {
     convert_to_us_units(n);
   }
-
   return n;
 }
 
@@ -75,22 +44,18 @@ function serving_water_use(serving, serving_amount, serving_unit, water_amount, 
 }
 
 function reset_content() {
+  console.log("Reset content");
   $(".water-footprint .result .heading").html("");
   $(".water-footprint .result .bulk-volume").html("").removeClass("primary");
   $(".water-footprint .result .bulk-weight").html("").removeClass("primary secondary");
   $(".water-footprint .result .one-serving").html("");
 }
 
-// Display water use on user selection
-
-$('.select-item').on('change', function (e) {
-  reset_content();
-  var choices = this;
-  var item_key = $(choices).val();
-
-  var item = foods[item_key];
+function update_displayed_content(item) {
+  console.log("Updating displayed content");
   var litre_info = item.water_use.water_litres;
-
+  console.log(item);
+  console.log(litre_info);
   $(".water-footprint .result .heading").html(item.food_name);
 
   var result;
@@ -111,4 +76,65 @@ $('.select-item').on('change', function (e) {
   } else {
     $(".water-footprint .result .no-info").html("<p>No information available.</p>");
   }
+}
+
+
+// Populates list of foods inside select
+$.each(foods, function(food, food_info) {
+  if (food_info.water_use.water_litres) {
+    $('.water-footprint .select-item').append(
+      "<option class='food-item " + food + "' value='" + food + "'>"
+      + food_info.food_name +
+      "</option>"
+    );
+  }
+});
+
+
+// Populates list of foods inside input
+var food_names = [];
+
+$.each(foods, function(food, food_info) {
+  if (food_info.water_use.water_litres) {
+    // food_names.push({label: food_info.food_name, value: food});
+    food_names.push(food_info.food_name);
+  }
+});
+
+$(".choose-item").autocomplete({
+  source: food_names,
+});
+
+
+// Display water use on user selection
+
+$('.select-item').on('change', function (e) {
+  reset_content();
+  var item_key = $(this).val();
+  var item = foods[item_key];
+
+  update_displayed_content(item);
+});
+
+$(".choose-item").change(function() {
+  reset_content();
+  var food_name = $(this).val();
+  var item;
+
+  $.each(foods, function(food, food_info) {
+    if (food_info.food_name === food_name) {
+      item = food_info;
+    }
+  });
+
+  // $.each(food_names, function(obj, keys) {
+  //   if (keys.label == food_name) {
+  //     food_key = keys.value;
+  //   }
+  // });
+
+  console.log(item);
+  // console.log(food_key);
+
+  update_displayed_content(item);
 });
