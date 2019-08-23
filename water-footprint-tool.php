31d78c90166e450ae980d5e3577dcfcf172d876a
Elizabeth Mitchell <elizabeth@sassafras.coop>
2019-08-23 07:49:53 -0400

    Update version

--- water-footprint-tool.php
+++ water-footprint-tool.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Water_Footprint
- * @version 0.2
+ * @version 2.0
  */
 /*
 Plugin Name: Water Footprint
 Plugin URI: http://github.com/emitche/water-footprint-wp/
 Description: A simple plugin to display the water footprint of specific foods.
 Author: Elizabeth Mitchell
-Version: 0.2
+Version: 2.0
 Author URI: http://github.com/emitche
 */
 
